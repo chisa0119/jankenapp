cfc386fbed95107ae608fd2d27c4b1410ace2ef2
八戸知里 <hachinohechisato@hachinohechirinoMacBook-Pro.local>
2020-06-22 11:47:07 +0900

    ページ追加

---
+++ src/views/Count.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="about">
+        <h1>カウント</h1>
+        <p>{{ count }}</p>
+        <p>
+            <button @click="increment">+</button>
+            <button @click="decrement">-</button>
+        </p>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {};// VueCLIを使って作るときはdataのなかにreturnをかく
+    },
+    methods:{
+        increment() {
+            this.$store.commit('increment')
+        },
+        decrement() {
+            this.$store.commit('decrement')
+        }
+    },
+    computed: {
+        count() {
+            return this.$store.getters.count;
+        }
+    }
+};
+</script>
---
+++ src/views/Score.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="about">
+    <h1>スコア表示</h1>
+  </div>
+</template>
+
+<script>
+export default {
+    computed: {
+        count() {
+            return this.$store.getters.count;
+        }
+    }
+};
+</script>
