62b8ac9398481ba0b22d5462af6ca3ecdb7c3040
八戸知里 <hachinohechisato@hachinohechirinoMacBook-Pro.local>
2020-06-22 11:49:31 +0900

    変更

--- src/views/Score.vue
+++ src/views/Score.vue
@@ -1,11 +1,15 @@
 <template>
   <div class="about">
     <h1>スコア表示</h1>
+    <p>{{ count }}</p>
   </div>
 </template>
 
 <script>
 export default {
+    data() {
+        return {};// VueCLIを使って作るときはdataのなかにreturnをかく
+    },
     computed: {
         count() {
             return this.$store.getters.count;
